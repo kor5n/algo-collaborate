4327b5916193ae821ba2dc0c9eda31a72946f062
Anton <96891115+antonfrog@users.noreply.github.com>
2024-03-16 12:19:00 +0300

    Исправил баг со всплывающим сообщением
    Но анимация была убрана, чтобы всплывающее сообщение работало нормально.

--- figma version/js/scripts.js
+++ figma version/js/scripts.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
     let total = 0;
 
+    $(".flash-message").toggle()
     $(".dropdown").hide();
 
     $("#dismiss-flash-message").click(() => {
-        $(".flash-message").css("opacity", "0")
-    })
+        $(".flash-message").toggle()
+    });
 
     $(".calc-btn").click(function() {
         $(this).next(".dropdown").toggle();
@@ -19,25 +20,25 @@ $(document).ready(function() {
     });
     
     $(".dropdown-btn").click(function() {
-        let parent = $(this).parent()
-        let calc_btn = parent.prev(".calc-btn")
-        let value = $(this).attr("data-value")
+        let parent = $(this).parent();
+        let calc_btn = parent.prev(".calc-btn");
+        let value = $(this).attr("data-value");
         
         if (calc_btn.find(".component-choice").text() === "Не выбрано") {
-            calc_btn.find(".component-choice").text(`Выбрано: ${$(this).find(".name").text()}`)
-            calc_btn.css("border-radius", "10px")
-            calc_btn.find("i").css("transform", "rotate(0deg)")
-            parent.toggle()
-            total += +value
-            $("#total-number").text(`Итого: ${total}₽`)
+            calc_btn.find(".component-choice").text(`Выбрано: ${$(this).find(".name").text()}`);
+            calc_btn.css("border-radius", "10px");
+            calc_btn.find("i").css("transform", "rotate(0deg)");
+            parent.toggle();
+            total += +value;
+            $("#total-number").text(`Итого: ${total}₽`);
         } else {
-            $(".flash-message").css("opacity", "100%")
-        }
+            $(".flash-message").toggle();
+        };
     });
     
     $("#clear-total-btn").click(() => {
         total = 0;
-        $(".component-choice").text("Не выбрано")
+        $(".component-choice").text("Не выбрано");
         $("#total-number").text(`Итого: ${total}₽`);
     });
-})
+});
